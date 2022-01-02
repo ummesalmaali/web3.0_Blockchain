cad762555c9fc733892b769e54e8db6c041ae265
Umme Salma <ummes3527@gmail.com>
2022-01-03 00:17:59 +0600

    tailwind added

---
+++ client/src/components/NavBar.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const NavBar = () => {
+  return (
+    <div>
+      <h2>Nav Bar is here</h2>
+    </div>
+  );
+};
+
+export default NavBar;
---
+++ client/src/components/Welcome.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Welcome = () => {
+  return (
+    <div>
+      <h1>Welcome to BlockChain Developer</h1>
+    </div>
+  );
+};
+
+export default Welcome;
